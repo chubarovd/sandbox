619ba19e25b7f1378a4c31492cc3cc5cf382b510
Dmitriy Chubarov <dmitriy.chubarov@nexign.com>
2019-09-04 15:30:55 +0300

    worked benchs

--- benchmarks/src/main/java/BenchmarkMain.java
+++ benchmarks/src/main/java/BenchmarkMain.java
@@ -1,5 +1,15 @@
+import benchs.CreateArrayListBench;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
 public class BenchmarkMain {
     public static void main(String[] args) throws Exception {
-        org.openjdk.jmh.Main.main(args);
+        Options opt = new OptionsBuilder()
+            .include(CreateArrayListBench.class.getSimpleName())
+            .forks(5)
+            .build();
+
+        new Runner(opt).run();
     }
 }
--- benchmarks/src/main/java/benchs/CreateArrayListBench.java
+++ benchmarks/src/main/java/benchs/CreateArrayListBench.java
@@ -1,51 +1,68 @@
 package benchs;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.Warmup;
-import org.openjdk.jmh.infra.Blackhole;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
 
-@BenchmarkMode(Mode.All)
-@Warmup(iterations = 1000)
-@Measurement(iterations = 1000000, batchSize = 10)
+@State(Scope.Thread)
+@BenchmarkMode({Mode.AverageTime})
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(value = 2)
+@Warmup(iterations = 2, batchSize = 1)
+@Measurement(iterations = 10, batchSize = 1)
 public class CreateArrayListBench {
-    private static final String VALUE = "any value";
 
-    @Benchmark
-    public void createSingletonListWithCollections(Blackhole bh) {
-        List<String> anySingleton = new ArrayList<>(Collections.singletonList(VALUE));
-        bh.consume(anySingleton);
-    }
+  private static final String VALUE = "any value";
 
-    @Benchmark
-    public void createSingletonListWithArrays() {
-        List<String> anySingleton = new ArrayList<>(Arrays.asList(VALUE));
-    }
+  @Benchmark
+  @Measurement(iterations = 1)
+  public void createSingletonListWithCollections(Blackhole bh) {
+    List<String> anySingleton = new ArrayList<>(Collections.singletonList(VALUE));
+    bh.consume(anySingleton);
+  }
 
-    @Benchmark
-    public void createSingletonListWithStream() {
-        List<String> anySingleton = Stream.of(VALUE).collect(Collectors.toList());
-    }
+  @Benchmark
+  @Measurement(iterations = 1)
+  public void createSingletonListWithArrays(Blackhole bh) {
+    List<String> anySingleton = new ArrayList<>(Arrays.asList(VALUE));
+    bh.consume(anySingleton);
+  }
 
-    @Benchmark
-    public void createSingletonListWithAnonymous() {
-        List<String> anySingleton = new ArrayList<String>() {{
-            add(VALUE);
-        }};
-    }
+  @Benchmark
+  @Measurement(iterations = 1)
+  public void createSingletonListWithStream(Blackhole bh) {
+    List<String> anySingleton = Stream.of(VALUE).collect(Collectors.toList());
+    bh.consume(anySingleton);
+  }
 
-    @Benchmark
-    public void createSingletonListWithSimpleInstance() {
-        List<String> anySingleton = new ArrayList<>();
-        anySingleton.add(VALUE);
-    }
+  @Benchmark
+  @Measurement(iterations = 1)
+  public void createSingletonListWithAnonymous(Blackhole bh) {
+    List<String> anySingleton = new ArrayList<String>() {{
+      add(VALUE);
+    }};
+    bh.consume(anySingleton);
+  }
+
+  @Benchmark
+  @Measurement(iterations = 1)
+  public void createSingletonListWithSimpleInstance(Blackhole bh) {
+    List<String> anySingleton = new ArrayList<>();
+    anySingleton.add(VALUE);
+    bh.consume(anySingleton);
+  }
 }
